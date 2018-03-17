1e0c606ccd2dbf14e9765dd44217b715d12a0fa7
floriciolacu <florentina.ciolacu@yahoo.com>
2018-03-17 11:14:01 +0200

    Created database

--- Restaurante/admin.py
+++ Restaurante/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from Restaurante.models import Restaurant, Keyword, UserProfile
+# Register your models here.
+
+admin.site.register(Restaurant)
+admin.site.register(Keyword)
+admin.site.register(UserProfile)
--- Restaurante/models.py
+++ Restaurante/models.py
@@ -1,3 +1,32 @@
+from __future__ import unicode_literals
+
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class Keyword(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length = 100)
+    location = models.CharField(max_length = 100)
+    website = models.CharField(max_length = 100)
+    specific = models.CharField(max_length = 100)
+    rating = models.FloatField(default=0.0)
+    keywords = models.ManyToManyField(Keyword, related_name='Keywords')
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile')
+    favourites = models.ManyToManyField(Restaurant, related_name='Favourites')
+    preferences = models.ManyToManyField(Keyword, related_name='Preferences')
+
+    def __str__(self):
+        return self.user.first_name + " " + self.user.last_name
