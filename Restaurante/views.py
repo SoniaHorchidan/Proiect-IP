3ce345faa476d3a5884f8f3df1d1271333d99d84
SoniaHorchidan <sonia.florina@yahoo.com>
2018-03-19 19:55:05 +0200

    added login

--- Restaurante/views.py
+++ Restaurante/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from Restaurante.forms import LoginForm
+from django.shortcuts import render, redirect
 
 
 def index(request):
     return HttpResponse("Server is running:)")
+
+def login_view(request):
+    context = {}
+    if request.method == 'GET':
+        form = LoginForm()
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password'])
+            if user:
+                login(request=request,
+                      user=user)
+                return redirect('index')
+            else:
+                context['error_message'] = 'Wrong username or password!'
+    context['form'] = form
+    return render(request, 'login.html', context)
+
+
+def logout_view(request):
+    if request.method == 'GET':
+        logout(request)
+        return redirect('login')
